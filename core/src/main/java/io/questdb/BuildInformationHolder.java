b7a15f9066930fefad0405eaa74bb5c7b5ea7b85
ajay <ajaychinna1433@gmail.com>
2023-07-06 18:15:35 +0530

    initial project setup

---
+++ core/src/main/java/io/questdb/BuildInformationHolder.java
@@ -0,0 +1,133 @@
+/*******************************************************************************
+ *     ___                  _   ____  ____
+ *    / _ \ _   _  ___  ___| |_|  _ \| __ )
+ *   | | | | | | |/ _ \/ __| __| | | |  _ \
+ *   | |_| | |_| |  __/\__ \ |_| |_| | |_) |
+ *    \__\_\\__,_|\___||___/\__|____/|____/
+ *
+ *  Copyright (c) 2014-2019 Appsicle
+ *  Copyright (c) 2019-2023 QuestDB
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
+
+package io.questdb;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
+
+public class BuildInformationHolder implements BuildInformation, CharSequence {
+    public static final BuildInformationHolder INSTANCE = fetchBuildInformation();
+    private static final String UNKNOWN = "Unknown Version";
+    private final String buildKey;
+    private final CharSequence commitHash;
+    private final CharSequence jdkVersion;
+    private final CharSequence questDbVersion;
+
+    public BuildInformationHolder() {
+        this(UNKNOWN, UNKNOWN, UNKNOWN);
+    }
+
+    public BuildInformationHolder(CharSequence questDbVersion, CharSequence commitHash, CharSequence jdkVersion) {
+        this.questDbVersion = questDbVersion;
+        this.commitHash = commitHash;
+        this.jdkVersion = jdkVersion;
+        this.buildKey = questDbVersion + ":" + commitHash + ":" + jdkVersion;
+    }
+
+    @Override
+    public char charAt(int index) {
+        return buildKey.charAt(index);
+    }
+
+    @Override
+    public CharSequence getCommitHash() {
+        return commitHash;
+    }
+
+    @Override
+    public CharSequence getJdkVersion() {
+        return jdkVersion;
+    }
+
+    @Override
+    public CharSequence getQuestDbVersion() {
+        return questDbVersion;
+    }
+
+    @Override
+    public int length() {
+        return buildKey.length();
+    }
+
+    @NotNull
+    @Override
+    public CharSequence subSequence(int start, int end) {
+        return buildKey.subSequence(start, end);
+    }
+
+    @Override
+    public String toString() {
+        return buildKey;
+    }
+
+    private static BuildInformationHolder fetchBuildInformation() {
+        try {
+            final Attributes manifestAttributes = getManifestAttributes();
+            return new BuildInformationHolder(
+                    getQuestDbVersion(manifestAttributes),
+                    getCommitHash(manifestAttributes),
+                    getJdkVersion(manifestAttributes)
+            );
+        } catch (IOException e) {
+            return new BuildInformationHolder();
+        }
+    }
+
+    private static CharSequence getCommitHash(final Attributes manifestAttributes) {
+        final CharSequence version = manifestAttributes.getValue("Build-Commit-Hash");
+        return version != null ? version : "Unknown Version";
+    }
+
+    private static CharSequence getJdkVersion(final Attributes manifestAttributes) {
+        final CharSequence version = manifestAttributes.getValue("Build-Jdk");
+        return version != null ? version : "Unknown Version";
+    }
+
+    private static Attributes getManifestAttributes() throws IOException {
+        final Enumeration<URL> resources = ServerMain.class.getClassLoader()
+                .getResources("META-INF/MANIFEST.MF");
+        while (resources.hasMoreElements()) {
+            try (InputStream is = resources.nextElement().openStream()) {
+                final Manifest manifest = new Manifest(is);
+                final Attributes attributes = manifest.getMainAttributes();
+                if ("org.questdb".equals(attributes.getValue("Implementation-Vendor-Id"))) {
+                    return manifest.getMainAttributes();
+                }
+            }
+        }
+        return new Attributes();
+    }
+
+    private static CharSequence getQuestDbVersion(final Attributes manifestAttributes) {
+        final CharSequence version = manifestAttributes.getValue("Implementation-Version");
+        return version != null ? version : "[DEVELOPMENT]";
+    }
+}
---
+++ core/src/main/java/io/questdb/ServerMain.java
@@ -0,0 +1,335 @@
+/*******************************************************************************
+ *     ___                  _   ____  ____
+ *    / _ \ _   _  ___  ___| |_|  _ \| __ )
+ *   | | | | | | |/ _ \/ __| __| | | |  _ \
+ *   | |_| | |_| |  __/\__ \ |_| |_| | |_) |
+ *    \__\_\\__,_|\___||___/\__|____/|____/
+ *
+ *  Copyright (c) 2014-2019 Appsicle
+ *  Copyright (c) 2019-2023 QuestDB
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
+
+package io.questdb;
+
+import io.questdb.cairo.CairoConfiguration;
+import io.questdb.cairo.CairoEngine;
+import io.questdb.cairo.ColumnIndexerJob;
+import io.questdb.cairo.O3Utils;
+import io.questdb.cairo.wal.ApplyWal2TableJob;
+import io.questdb.cairo.wal.CheckWalTransactionsJob;
+import io.questdb.cairo.wal.WalPurgeJob;
+import io.questdb.cutlass.Services;
+import io.questdb.cutlass.auth.AuthenticatorFactory;
+import io.questdb.cutlass.auth.DefaultAuthenticatorFactory;
+import io.questdb.cutlass.auth.EllipticCurveAuthenticatorFactory;
+import io.questdb.cutlass.text.CopyJob;
+import io.questdb.cutlass.text.CopyRequestJob;
+import io.questdb.griffin.DatabaseSnapshotAgent;
+import io.questdb.griffin.FunctionFactory;
+import io.questdb.griffin.FunctionFactoryCache;
+import io.questdb.griffin.engine.groupby.vect.GroupByJob;
+import io.questdb.griffin.engine.table.AsyncFilterAtom;
+import io.questdb.griffin.engine.table.LatestByAllIndexedJob;
+import io.questdb.log.Log;
+import io.questdb.log.LogFactory;
+import io.questdb.mp.WorkerPool;
+import io.questdb.std.Misc;
+import io.questdb.std.ObjList;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.Closeable;
+import java.io.File;
+import java.util.ServiceLoader;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class ServerMain implements Closeable {
+    private final String banner;
+    private final AtomicBoolean closed = new AtomicBoolean();
+    private final ServerConfiguration config;
+    private final CairoEngine engine;
+    private final FunctionFactoryCache ffCache;
+    private final ObjList<Closeable> freeOnExitList = new ObjList<>();
+    private final Log log;
+    private final AtomicBoolean running = new AtomicBoolean();
+    private final WorkerPoolManager workerPoolManager;
+
+    public ServerMain(String... args) {
+        this(new Bootstrap(args));
+    }
+
+    public ServerMain(final Bootstrap bootstrap) {
+        this(bootstrap.getConfiguration(), bootstrap.getMetrics(), bootstrap.getLog(), bootstrap.getBanner());
+    }
+
+    public ServerMain(final ServerConfiguration config, final Metrics metrics, final Log log, String banner) {
+        this.config = config;
+        this.log = log;
+        this.banner = banner;
+
+        // create cairo engine
+        final CairoConfiguration cairoConfig = config.getCairoConfiguration();
+        engine = freeOnExit(new CairoEngine(cairoConfig, metrics));
+
+        // create function factory cache
+        ffCache = new FunctionFactoryCache(
+                cairoConfig,
+                ServiceLoader.load(FunctionFactory.class, FunctionFactory.class.getClassLoader())
+        );
+
+        config.init(engine, ffCache);
+
+        // snapshots
+        final DatabaseSnapshotAgent snapshotAgent = freeOnExit(new DatabaseSnapshotAgent(engine));
+
+        // create the worker pool manager, and configure the shared pool
+        final boolean walSupported = config.getCairoConfiguration().isWalSupported();
+        final boolean isReadOnly = config.getCairoConfiguration().isReadOnlyInstance();
+        final boolean walApplyEnabled = config.getCairoConfiguration().isWalApplyEnabled();
+        workerPoolManager = new WorkerPoolManager(config, metrics.health()) {
+            @Override
+            protected void configureSharedPool(WorkerPool sharedPool) {
+                try {
+                    sharedPool.assign(engine.getEngineMaintenanceJob());
+
+                    final MessageBus messageBus = engine.getMessageBus();
+                    // register jobs that help parallel execution of queries and column indexing.
+                    sharedPool.assign(new ColumnIndexerJob(messageBus));
+                    sharedPool.assign(new GroupByJob(messageBus));
+                    sharedPool.assign(new LatestByAllIndexedJob(messageBus));
+
+                    if (!isReadOnly) {
+                        O3Utils.setupWorkerPool(
+                                sharedPool,
+                                engine,
+                                config.getCairoConfiguration().getCircuitBreakerConfiguration(),
+                                ffCache
+                        );
+
+                        if (walSupported) {
+                            sharedPool.assign(new CheckWalTransactionsJob(engine));
+                            final WalPurgeJob walPurgeJob = new WalPurgeJob(engine);
+                            snapshotAgent.setWalPurgeJobRunLock(walPurgeJob.getRunLock());
+                            walPurgeJob.delayByHalfInterval();
+                            sharedPool.assign(walPurgeJob);
+                            sharedPool.freeOnExit(walPurgeJob);
+
+                            if (walApplyEnabled && !config.getWalApplyPoolConfiguration().isEnabled()) {
+                                setupWalApplyJob(sharedPool, engine, getSharedWorkerCount(), ffCache);
+                            }
+                        }
+
+                        // text import
+                        CopyJob.assignToPool(messageBus, sharedPool);
+                        if (cairoConfig.getSqlCopyInputRoot() != null) {
+                            final CopyRequestJob copyRequestJob = new CopyRequestJob(
+                                    engine,
+                                    // save CPU resources for collecting and processing jobs
+                                    Math.max(1, sharedPool.getWorkerCount() - 2),
+                                    ffCache
+                            );
+                            sharedPool.assign(copyRequestJob);
+                            sharedPool.freeOnExit(copyRequestJob);
+                        }
+                    }
+
+                    // telemetry
+                    if (!cairoConfig.getTelemetryConfiguration().getDisableCompletely()) {
+                        final TelemetryJob telemetryJob = new TelemetryJob(engine, ffCache);
+                        freeOnExitList.add(telemetryJob);
+                        if (cairoConfig.getTelemetryConfiguration().getEnabled()) {
+                            sharedPool.assign(telemetryJob);
+                        }
+                    }
+                } catch (Throwable thr) {
+                    throw new Bootstrap.BootstrapException(thr);
+                }
+            }
+        };
+
+        if (walApplyEnabled && !isReadOnly && walSupported && config.getWalApplyPoolConfiguration().isEnabled()) {
+            WorkerPool walApplyWorkerPool = workerPoolManager.getInstance(
+                    config.getWalApplyPoolConfiguration(),
+                    metrics.health(),
+                    WorkerPoolManager.Requester.WAL_APPLY
+            );
+            setupWalApplyJob(walApplyWorkerPool, engine, workerPoolManager.getSharedWorkerCount(), ffCache);
+        }
+
+        // http
+        freeOnExit(Services.createHttpServer(
+                config.getHttpServerConfiguration(),
+                engine,
+                workerPoolManager,
+                ffCache,
+                snapshotAgent,
+                metrics
+        ));
+
+        // http min
+        freeOnExit(Services.createMinHttpServer(
+                config.getHttpMinServerConfiguration(),
+                engine,
+                workerPoolManager,
+                metrics
+        ));
+
+        // pg wire
+        freeOnExit(Services.createPGWireServer(
+                config.getPGWireConfiguration(),
+                engine,
+                workerPoolManager,
+                ffCache,
+                snapshotAgent,
+                metrics
+        ));
+
+        if (!isReadOnly) {
+            // ilp/tcp
+            freeOnExit(Services.createLineTcpReceiver(
+                    config.getLineTcpReceiverConfiguration(),
+                    engine,
+                    workerPoolManager,
+                    metrics
+            ));
+
+            // ilp/udp
+            freeOnExit(Services.createLineUdpReceiver(
+                    config.getLineUdpReceiverConfiguration(),
+                    engine,
+                    workerPoolManager
+            ));
+        }
+
+        System.gc(); // GC 1
+        log.advisoryW().$("server is ready to be started").$();
+    }
+
+    public static AuthenticatorFactory getAuthenticatorFactory(ServerConfiguration configuration) {
+        AuthenticatorFactory authenticatorFactory;
+        // create default authenticator for Line TCP protocol
+        if (configuration.getLineTcpReceiverConfiguration().isEnabled() && configuration.getLineTcpReceiverConfiguration().getAuthDB() != null) {
+            // we need "root/" here, not "root/db/"
+            final String rootDir = new File(configuration.getCairoConfiguration().getRoot()).getParent();
+            authenticatorFactory = new EllipticCurveAuthenticatorFactory(
+                    configuration.getLineTcpReceiverConfiguration().getNetworkFacade(),
+                    new File(rootDir, configuration.getLineTcpReceiverConfiguration().getAuthDB()).getAbsolutePath()
+            );
+        } else {
+            authenticatorFactory = DefaultAuthenticatorFactory.INSTANCE;
+        }
+        return authenticatorFactory;
+    }
+
+    public static void main(String[] args) {
+        try {
+            new ServerMain(args).start(true);
+        } catch (Throwable thr) {
+            thr.printStackTrace();
+            LogFactory.closeInstance();
+            System.exit(55);
+        }
+    }
+
+    @Override
+    public void close() {
+        if (closed.compareAndSet(false, true)) {
+            workerPoolManager.halt();
+            // free instances in reverse order to which we allocated them
+            for (int i = freeOnExitList.size() - 1; i >= 0; i--) {
+                Misc.free(freeOnExitList.getQuick(i));
+            }
+            freeOnExitList.clear();
+        }
+    }
+
+    public ServerConfiguration getConfiguration() {
+        return config;
+    }
+
+    public CairoEngine getEngine() {
+        if (closed.get()) {
+            throw new IllegalStateException("close was called");
+        }
+        return engine;
+    }
+
+    public WorkerPoolManager getWorkerPoolManager() {
+        if (closed.get()) {
+            throw new IllegalStateException("close was called");
+        }
+        return workerPoolManager;
+    }
+
+    public boolean hasBeenClosed() {
+        return closed.get();
+    }
+
+    public boolean hasStarted() {
+        return running.get();
+    }
+
+    public void start() {
+        start(false);
+    }
+
+    public void start(boolean addShutdownHook) {
+        if (!closed.get() && running.compareAndSet(false, true)) {
+            if (addShutdownHook) {
+                addShutdownHook();
+            }
+            workerPoolManager.start(log);
+            Bootstrap.logWebConsoleUrls(config, log, banner);
+            System.gc(); // final GC
+            log.advisoryW().$("enjoy").$();
+        }
+    }
+
+    private void addShutdownHook() {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                System.err.println("QuestDB is shutting down...");
+                System.err.println("Pre-touch magic number: " + AsyncFilterAtom.PRE_TOUCH_BLACK_HOLE.sum());
+                close();
+                LogFactory.closeInstance();
+            } catch (Error ignore) {
+                // ignore
+            } finally {
+                System.err.println("QuestDB is shutdown.");
+            }
+        }));
+    }
+
+    protected <T extends Closeable> T freeOnExit(T closeable) {
+        if (closeable != null) {
+            freeOnExitList.add(closeable);
+        }
+        return closeable;
+    }
+
+    protected void setupWalApplyJob(
+            WorkerPool workerPool,
+            CairoEngine engine,
+            int sharedWorkerCount,
+            @Nullable FunctionFactoryCache ffCache
+    ) {
+        for (int i = 0, workerCount = workerPool.getWorkerCount(); i < workerCount; i++) {
+            // create job per worker
+            final ApplyWal2TableJob applyWal2TableJob = new ApplyWal2TableJob(engine, workerCount, sharedWorkerCount, ffCache);
+            workerPool.assign(i, applyWal2TableJob);
+            workerPool.freeOnExit(applyWal2TableJob);
+        }
+    }
+}
